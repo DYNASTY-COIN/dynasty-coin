b7eb441822f684a4b4b1eca8e47ab3509dcde433
DynastyCoin <149406635+DYNASTY-COIN@users.noreply.github.com>
2023-11-08 22:02:23 +0700

    Add files via upload

---
+++ scripts/utils.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function checkclientversion()
+{
+	parityversionoutput=$(../openethereum --version)
+
+	parityversion=$(echo $parityversionoutput | cut -d'-' -f 2 | cut -d'/' -f 2 | cut -d'v' -f 2) 
+	major=$(echo $parityversion | cut -d'.' -f 1)
+	minor=$(echo $parityversion | cut -d'.' -f 2)
+	patch=$(echo $parityversion | cut -d'.' -f 3)
+
+	majormin=2
+	minormin=6
+	patchmin=5
+
+	errormsg="Parity Ethereum client version should be more or equal to v2.6.5. Your current installation has version $parityversion"
+
+	function genError()
+	{
+		echo $errormsg
+		exit 0
+	}
+
+	if [ $major -lt $majormin ];
+	then
+		genError
+	elif [ $major -eq $majormin ] && [ $minor -lt $minormin ];
+	then
+		genError
+	elif [ $major -eq $majormin ] && [ $minor -eq $minormin ] && [ $patch -lt $patchmin ];
+	then
+		genError
+	fi
+
+	echo ""
+	exit 0
+}
